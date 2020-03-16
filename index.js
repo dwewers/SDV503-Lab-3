4e2a9571e09e1f52219afc5145956bf4091a978a
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-17 10:47:11 +1300

    Working without if

--- index.js
+++ index.js
@@ -1,76 +1,58 @@
 // var test_result;
-const score = 1;
+const score = 25;
 
 
-switch (score) {
-  case 1:
-    if (score <= 100 && score >= 85) {
-     var test_result = "A_Plus";
-      console.log("A+ Distinction");
-    }
+switch (true) {
+  case  score <= 100 && score >= 85:{
+     var test_result = "A+ Distinction";
+      console.log(test_result);
     break;
-
-  case 2:
-    if (score <= 84 && score >= 80) {
-      var test_result = "A";
-      console.log("A Distinction");
-    }
+  }
+  case score <= 84 && score >= 80 :{
+      var test_result = "A+ Distinction";
+      console.log(test_result);
     break;
-
-  case 3:
-    if (score <= 79 && score >= 75) {
-      test_result = A_Minus;
-      console.log("A- Distinction");
-    }
+  }
+  case score <= 79 && score >= 75 :{
+      test_result = "A- Distinction";
+      console.log(test_result);
     break;
-
-  case 4:
-    if (score <= 74 && score >= 70) {
-      test_result = B_Plus;
-      console.log("B+ - Merit");
-    }
+  }
+  case score <= 74 && score >= 70: {
+      test_result = "B+ Merit";
+      console.log(test_result);
     break;
-
-  case 5:
-    if (score <= 69 && score >= 65) {
-      test_result = B;
-      console.log("B merit");
-    }
+  }
+  case score <= 69 && score >= 65: {
+      test_result = "B merit";
+      console.log(test_result);
     break;
-
-  case 6:
-    if (score <= 64 && score >= 60) {
-      test_result = B_Minus;
-      console.log("B- Merit");
-    }
+  }
+  case score <= 64 && score >= 60: {
+      test_result = "B- Merit";
+      console.log(test_result);
     break;
-
-  case 7:
-    if (score <= 59 && score >= 55) {
-      test_result = C_Plus;
-      console.log("C+ Pass");
-    }
+  }
+  case score <= 59 && score >= 55: {
+      test_result = "C+ Merit";
+      console.log(test_result);
     break;
-
-  case 8:
-    if (score <= 54 && score >= 50) {
-      test_result = C;
-      console.log("C Pass");
-    }
+  }
+  case score <= 54 && score >= 50: {
+      test_result = "C Pass";
+      console.log(test_result);
     break;
-
-  case 9:
-    if (score <= 49 && score >= 40) {
-      test_result = D;
-      console.log("D Fail");
-    }
+  }
+  case score <= 49 && score >= 40: {
+      test_result = "D Fail";
+      console.log(test_result);
     break;
-
-  case 10:
-    if (score <= 39 && score >= 0) {
-      test_result = E;
-      console.log("E Fail");
-    }
+  }
+  case score <= 39 && score >= 0: {
+      var test_result = "E Fail";
+      console.log(test_result);
     break;
+  }
+
 }
 
