4a2e9d9bbcefd656c3c00dee6f59f1217b453be4
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-17 10:54:56 +1300

    with if statements

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-// var test_result;
-const score = 25;
+  // var test_result;
+  const score = 25;
 
 
-switch (true) {
+  switch (true) {
   case  score <= 100 && score >= 85:{
-     var test_result = "A+ Distinction";
-      console.log(test_result);
-    break;
+  var test_result = "A+";
+  console.log(test_result);
+  break;
   }
   case score <= 84 && score >= 80 :{
-      var test_result = "A+ Distinction";
-      console.log(test_result);
-    break;
+  var test_result = "A";
+  console.log(test_result);
+  break;
   }
   case score <= 79 && score >= 75 :{
-      test_result = "A- Distinction";
-      console.log(test_result);
-    break;
+  test_result = "A-";
+  console.log(test_result);
+  break;
   }
   case score <= 74 && score >= 70: {
-      test_result = "B+ Merit";
-      console.log(test_result);
-    break;
+  test_result = "B+";
+  console.log(test_result);
+  break;
   }
   case score <= 69 && score >= 65: {
-      test_result = "B merit";
-      console.log(test_result);
-    break;
+  test_result = "B";
+  console.log(test_result);
+  break;
   }
   case score <= 64 && score >= 60: {
-      test_result = "B- Merit";
-      console.log(test_result);
-    break;
+  test_result = "B-";
+  console.log(test_result);
+  break;
   }
   case score <= 59 && score >= 55: {
-      test_result = "C+ Merit";
-      console.log(test_result);
-    break;
+  test_result = "C+";
+  console.log(test_result);
+  break;
   }
   case score <= 54 && score >= 50: {
-      test_result = "C Pass";
-      console.log(test_result);
-    break;
+  test_result = "C";
+  console.log(test_result);
+  break;
   }
   case score <= 49 && score >= 40: {
-      test_result = "D Fail";
-      console.log(test_result);
-    break;
+  test_result = "D";
+  console.log(test_result);
+  break;
   }
   case score <= 39 && score >= 0: {
-      var test_result = "E Fail";
-      console.log(test_result);
-    break;
+  var test_result = "E";
+  console.log(test_result);
+  break;
   }
 
-}
+  if (test_result <=100 && test_result >= 75){
+  console.log("Distinction");
+  }
+  else if (test_result <=74 && test_result >= 60){
+  console.log("Distinction");
+  }
+  else if (test_result <=59 && test_result >= 50){
+  console.log("Distinction");
+  }
+  else (test_result <=49){
+  console.log("Fail");
+  }
+  }
+
 
