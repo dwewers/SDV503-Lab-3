79dfff9e2c90f417e7470f9b2c6819e68a128159
dwewers <61395921+dwewers@users.noreply.github.com>
2020-03-17 17:43:56 +1300

    Comments on lab

--- index.js
+++ index.js
@@ -1,58 +1,57 @@
 // var test_result;
-const score = 76;
-var test_result;
+const score = 76; //Creates a variable, for which we can input the score and use it later on in the code
+var test_result; //Creates a variable that we can use to identify what the result is based off the score
 
-switch (true) {
-  case score <= 100 && score >= 85: {
-    let test_result = "A+";
-    console.log(test_result);
-    break;
+switch (true) { //This is known as the switch block. Inside it consists of what is known as a 'switch keyword'
+  case score <= 100 && score >= 85: { //Compares the values after to score
+    let test_result = "A+"; //What we are saying, is that if it comes back that the previous statement is true, test_result is A+
+    console.log(test_result); //logs variable test_result if the statement is correct
+    break; //this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 84 && score >= 80: {
-    let test_result = "A";
-    console.log(test_result);
-    break;
+  case score <= 84 && score >= 80: {//Compares the values after to score
+    let test_result = "A";//What we are saying, is that if it comes back that the previous statement is true, test_result is A
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 79 && score >= 75: {
-    let test_result = "A-";
-    console.log(test_result);
-    break;
+  case score <= 79 && score >= 75: {//Compares the values after to score
+    let test_result = "A-";//What we are saying, is that if it comes back that the previous statement is true, test_result is A-
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 74 && score >= 70: {
-    let test_result = "B+";
-    console.log(test_result);
-    break;
+  case score <= 74 && score >= 70: {//Compares the values after to score
+    let test_result = "B+";//What we are saying, is that if it comes back that the previous statement is true, test_result is B+
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 69 && score >= 65: {
-    let test_result = "B";
-    console.log(test_result);
-    break;
+  case score <= 69 && score >= 65: {//Compares the values after to score
+    let test_result = "B";//What we are saying, is that if it comes back that the previous statement is true, test_result is B
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 64 && score >= 60: {
-    let test_result = "B-";
-    console.log(test_result);
-    break;
+  case score <= 64 && score >= 60: {//Compares the values after to score
+    let test_result = "B-";//What we are saying, is that if it comes back that the previous statement is true, test_result is B-
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 59 && score >= 55: {
-    let test_result = "C+";
-    console.log(test_result);
-    break;
+  case score <= 59 && score >= 55: {//Compares the values after to score
+    let test_result = "C+";//What we are saying, is that if it comes back that the previous statement is true, test_result is C+
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 54 && score >= 50: {
-    let test_result = "C";
-    console.log(test_result);
-    break;
+  case score <= 54 && score >= 50: {//Compares the values after to score
+    let test_result = "C";//What we are saying, is that if it comes back that the previous statement is true, test_result is C
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 49 && score >= 40: {
-    let test_result = "D";
-    console.log(test_result);
-    break;
+  case score <= 49 && score >= 40: {//Compares the values after to score
+    let test_result = "D";//What we are saying, is that if it comes back that the previous statement is true, test_result is D
+    console.log(test_result);//logs variable test_result if the statement is correct
+    break;//this is used so that when we get to this part, we break from the switch block
   }
-  case score <= 39 && score >= 0:
-    {
-      let test_result = "E";
-      console.log(test_result);
-      break;
+  case score <= 39 && score >= 0: { //Compares the values after to score  
+      let test_result = "E";//What we are saying, is that if it comes back that the previous statement is true, test_result is E
+      console.log(test_result);//logs variable test_result if the statement is correct
+      break;//this is used so that when we get to this part, we break from the switch block
     }
 
     if (test_result <= 100 && test_result >= 75) {
